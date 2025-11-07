b5229190cff6d7c60cc24a9c962bd8fded87ecee
Maliha-Fatima916 <fatimamaliha024@gmail.com>
2025-11-07 19:07:29 +0500

    Implement built-in commands (exit, cd, help, jobs)

---
+++ src/main.c
@@ -0,0 +1,26 @@
+#include "shell.h"
+
+int main() {
+    char* cmdline;
+    char** arglist;
+
+    while ((cmdline = read_cmd(PROMPT, stdin)) != NULL) {
+        if ((arglist = tokenize(cmdline)) != NULL) {
+            // NEW: Check if it's a built-in command first
+            if (handle_builtin(arglist) == 0) {
+                // If not built-in, execute as external command
+                execute(arglist);
+            }
+
+            // Free the memory allocated by tokenize()
+            for (int i = 0; arglist[i] != NULL; i++) {
+                free(arglist[i]);
+            }
+            free(arglist);
+        }
+        free(cmdline);
+    }
+
+    printf("\nShell exited.\n");
+    return 0;
+}
